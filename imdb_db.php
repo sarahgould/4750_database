da20f1086f441a2b5d95f7b4882e5793fcc0a41a
samgalletta <82743003+samgalletta@users.noreply.github.com>
2022-04-13 16:29:38 -0400

    Update imdb_db.php

--- imdb_db.php
+++ imdb_db.php
@@ -7,6 +7,42 @@ function addMovie($title, $year, $gross, $runtime, $rating, $director) {
     $statement = $db->query($query);
 }
 
+function registerUser($username, $email, $password) {
+    if($username == ""){
+        echo "<p style='font-size: 20px; color: white'>" . "Enter a valid username" . "</p>";
+    }
+    else if($email == ""){
+        echo "<p style='font-size: 20px; color: white'>" . "Enter a valid email" . "</p>";
+    }
+    else if($password == ""){
+        echo "<p style='font-size: 20px; color: white'>" . "Enter a valid password" . "</p>";
+    }
+    else if(strlen($password) < 6){
+        echo "<p style='font-size: 20px; color: white'>" . "Password must be at least 6 characters" . "</p>";
+    }
+    else{
+    global $db;
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    
+    $query = "insert ignore into User 
+    values('" . $username . "', '" . $email . "', '" . $password . "')";
+
+    $statement = $db->query($query);
+    header('Location: login.php');
+    }
+}
+
+function submitComment($content, $title){
+    global $db;    
+    //echo $_SESSION['username'];
+    $username = $_SESSION['username'];
+    echo $username;
+    $query = "insert into Comment 
+    values('" . $username . "','".$content."','" . $title . "')";
+
+    $statement = $db->query($query);
+    
+}
 // function addComment($user, $title, $content) {
 //     // db handler
 //     global $db;
