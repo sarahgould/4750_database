a9364f8ec0f1e8d0b12dbc88d4c5c4a877f197b0
sarahgould <70811282+sarahgould@users.noreply.github.com>
2022-04-15 11:29:53 -0400

    Add files via upload

---
+++ imdb_db.php
@@ -0,0 +1,223 @@
+<?php
+
+function addMovie($title, $year, $gross, $runtime, $rating, $director) {
+    global $db;
+    $query = "insert ignore into Movies 
+    values('" . $title . "', " . $year ." , '" . $gross . "', " . $runtime ." , " . $rating ." , '" . $director . "')";
+    $statement = $db->query($query);
+}
+
+function registerUser($username, $email, $password) {
+    if($username == ""){
+        echo "<p style='font-size: 20px; color: white'>" . "Enter a valid username" . "</p>";
+    }
+    else if($email == ""){
+        echo "<p style='font-size: 20px; color: white'>" . "Enter a valid email" . "</p>";
+    }
+    else if($password == ""){
+        echo "<p style='font-size: 20px; color: white'>" . "Enter a valid password" . "</p>";
+    }
+    else if(strlen($password) < 6){
+        echo "<p style='font-size: 20px; color: white'>" . "Password must be at least 6 characters" . "</p>";
+    }
+    else{
+    global $db;
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    
+    $query = "insert ignore into User 
+    values('" . $username . "', '" . $email . "', '" . $password . "')";
+
+    $statement = $db->query($query);
+    header('Location: login.php');
+    }
+}
+
+function submitComment($content, $title, $series_title){
+    global $db;    
+    //echo $_SESSION['username'];
+    $username = $_SESSION['username'];
+    echo $username;
+    $query = "insert into Comment 
+    values('" . $username . "','" . $series_title . "','".$content."','" . $title . "');
+    insert into belongTo values('" . $series_title . "','" . $username . "','".$title."','" . $content . "')";
+    $statement = $db->query($query);
+    
+}
+
+function getComment_byTitle($title) {
+    // db handler
+    global $db;
+    // sql
+    $query = "select * from Comment where Series_Title = :title";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':title', $title);
+    $statement->execute();
+
+    $results = $statement->fetchAll();   
+    $statement->closeCursor();
+    return $results;
+}
+
+function getAllMovies() {
+	global $db;
+	$query = "select * from Movies order by IMDB_Rating desc";
+	$statement = $db->query($query); 
+        
+    // fetchAll() returns an array of all rows in the result set
+    $results = $statement->fetchAll();   
+    $statement->closeCursor();
+    return $results;
+}
+
+function getMovie_byTitle($title) {
+	global $db;
+	$query = "select * from Movies where Series_Title = :title";
+
+	$statement = $db->prepare($query);
+	$statement->bindValue(':title', $title);
+	$statement->execute();
+
+	// fetch() returns a row
+	$results = $statement->fetch();   
+	$statement->closeCursor();
+
+	return $results;	
+}
+
+function updateMovie($title, $year, $gross, $runtime, $rating, $director) {
+    global $db;
+    $query = "update Movies set Released_Year=:year, Gross=:gross, Runtime=:runtime, IMDB_Rating=:rating, Director=:director where Series_Title=:title";
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':year', $year);
+    $statement->bindValue(':gross', $gross);
+    $statement->bindValue(':runtime', $runtime);
+    $statement->bindValue(':rating', $rating);
+    $statement->bindValue(':director', $director);
+    $statement->bindValue(':title', $title);
+    $statement->execute();
+
+	$statement->closeCursor();
+
+}
+
+function deleteMovie($title) {
+    global $db;
+    $query = "delete from Movies where Series_Title=:title;
+    delete from featuredFor where Series_Title=:title;
+    delete from categorizedAs where Series_Title=:title;
+    delete from starsIn where Series_Title=:title";
+
+    $statement = $db->prepare($query);
+	$statement->bindValue(':title', $title);
+	$statement->execute();
+
+	$statement->closeCursor();
+
+}
+
+function getPoster_byTitle($title) {
+    global $db;
+    $query = "select * from featuredFor where Series_Title=:title";
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':title', $title);
+    $statement->execute();
+
+    $results = $statement->fetch();   
+    $statement->closeCursor();
+    return $results;
+}
+
+function getStarNames_byTitle($title) {
+    global $db;
+    $query = "select * from starsIn where Series_Title=:title";
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':title', $title);
+    $statement->execute();
+
+    $results = $statement->fetchAll();   
+    $statement->closeCursor();
+    return $results;
+}
+
+function getGenres_byTitle($title) {
+    global $db;
+    $query = "select * from categorizedAs where Series_Title=:title";
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':title', $title);
+    $statement->execute();
+
+    $results = $statement->fetchAll();   
+    $statement->closeCursor();
+    return $results;   
+}
+
+function addStarName($starName, $title) {
+    global $db;
+    $query = "insert ignore into starsIn values( '" . $title . "', '" . $starName . "');
+    insert ignore into MovieStars values(NULL, '" . $starName . "')";
+    $statement = $db->query($query);
+}
+
+function addGenre($title, $genreType) {
+    global $db;
+    $query = "insert ignore into categorizedAs values( '" . $title . "', '" . $genreType . "');
+    insert ignore into MovieGenre values(NULL, '" . $genreType . "')";
+    $statement = $db->query($query); 
+}
+
+function addPosterLink($posterLink, $title) {
+    global $db;
+    $query = "insert ignore into featuredFor values( '" . $posterLink . "', '" . $title . "')";
+    $statement = $db->query($query);
+}
+
+function deleteStarName($starName, $title) {
+    global $db;
+    $query = "delete from starsIn where starName=:starName and Series_Title=:title";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':title', $title);
+    $statement->bindValue(':starName', $starName);
+    $statement->execute();
+}
+
+function deleteGenre($title, $genreType) {
+    global $db;
+    $query = "delete from categorizedAs where genreType=:genreType and Series_Title=:title";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':title', $title);
+    $statement->bindValue(':genreType', $genreType);
+    $statement->execute();
+}
+
+function deletePosterLink($posterLink, $title) {
+    global $db;
+    $query = "delete from featuredFor where Poster_Link=:posterLink and Series_Title=:title";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':title', $title);
+    $statement->bindValue(':posterLink', $posterLink);
+    $statement->execute();
+}
+
+function getSearchedMovies($titleFilter) {
+    global $db;
+	$query = "select * from Movies where Series_Title like '%" . $titleFilter . "%'";
+	$statement = $db->query($query); 
+        
+    // fetchAll() returns an array of all rows in the result set
+    $results = $statement->fetchAll();   
+    $statement->closeCursor();
+    return $results;
+}
+
+function loginUser($username, $email, $password) {
+    global $db;
+    $query = "insert ignore into User 
+    values('" . $username . "', '" . $email . "', '" . $password . "')";
+    $statement = $db->query($query);
+}
+
+?>
---
+++ login.php
@@ -0,0 +1,139 @@
+<?php
+
+require('connect-imdb.php');
+require('imdb_db.php');
+
+?>
+
+<div style="margin-left: 25px">
+<?php
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if(!empty($_POST['btnAction']) && $_POST['btnAction'] == "Login") {
+            global $db;
+            $uname = mysqli_real_escape_string($link,$_POST['username']);
+            $password = mysqli_real_escape_string($link,$_POST['password']);
+          
+            $query = "select * from User where username='".$uname."'";
+            $res = mysqli_query($link,$query);
+            $userrow = mysqli_fetch_array($res);
+            $hashed_password = $userrow['password'];
+
+            if ($uname != "" && $password != ""){
+                
+                $sql_query = "select count(*) as cntUser from User where username='".$uname."'";
+                $result = mysqli_query($link,$sql_query);
+                $row = mysqli_fetch_array($result);
+
+                $count = $row['cntUser'];
+                
+                if(password_verify($password, $hashed_password)){
+                if($count > 0){
+                    
+                    $_SESSION['username'] = $uname;
+                    header('Location: sampleImdb.php');
+                    }
+                }else{
+                    echo "<p style='font-size: 20px; color: white'>" . "Invalid Username and/or Password" . "</p>";
+                }   
+
+            }   
+
+        }   
+}
+?>
+</div>
+
+<!-- 1. create HTML5 doctype -->
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">  
+  
+  <!-- 2. include meta tag to ensure proper rendering and touch zooming -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- 
+  Bootstrap is designed to be responsive to mobile.
+  Mobile-first styles are part of the core framework.
+   
+  width=device-width sets the width of the page to follow the screen-width
+  initial-scale=1 sets the initial zoom level when the page is first loaded   
+  -->
+  
+  <meta name="author" content="sarah gould">
+  <meta name="description" content="include some description about your page">  
+    
+  <title>Sample IMDB</title>
+  
+  <!-- 3. link bootstrap -->
+  <!-- if you choose to use CDN for CSS bootstrap -->  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  
+  <!-- you may also use W3's formats -->
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  
+  <!-- 
+  Use a link tag to link an external resource.
+  A rel (relationship) specifies relationship between the current document and the linked resource. 
+  -->
+  
+  <!-- If you choose to use a favicon, specify the destination of the resource in href -->
+  <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
+  
+  <!-- if you choose to download bootstrap and host it locally -->
+  <!-- <link rel="stylesheet" href="path-to-your-file/bootstrap.min.css" /> --> 
+  
+  <!-- include your CSS -->
+  <!-- <link rel="stylesheet" href="custom.css" />  -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  
+  <!--  open the dropdown if movie fields need to be updated-->
+  <!-- reference: https://stackoverflow.com/questions/58911249/maintain-colllapse-state-after-refresh -->
+  <script>
+      $(function() {
+        const collapseTrigger = $("#content");
+        if(<?php echo $open_menu; ?> != null) {
+            collapseTrigger.collapse("show");
+        }
+        
+      });
+  </script>    
+  
+  <style>
+    h2 {font-size: 40px; font-family: Impact; color: #F6BE00; text-align: center;}
+    b {color: white;}
+    
+    .wrapper{ margin: 0 auto; width: 360px; padding: 20px; }
+    
+    </style>
+
+
+</head>
+ 
+    
+
+    <body style="background-color: black;">
+
+    <h2 > Login to IMDB Movie Guide!</h2>
+    <div class="wrapper">
+        <form action="login.php" method="post" id="login User">
+
+        <input type="text" placeholder="Username" class="form-control" name="username" id="username">
+            <br/>
+            <input type="password" placeholder="Password" class="form-control" name="password" id="password">
+            <br/>
+            <div style="text-align: center;">
+                <input type="submit" value="Login" name="btnAction" class="btn btn-info"
+                title = "login user" />
+            </div>
+
+        </form>
+    </div>
+    <p style="font-size: 20px; color: white; margin-left: 25px">Don't have an account? <a href="register.php">Sign up here</a>.</p>
+    </body>
+    <hr/>
+</html>
